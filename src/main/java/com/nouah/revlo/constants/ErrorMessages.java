d24658732d30ba880e7316022dc6d7d4cf40fc0d
Greatnex1 <noahgreatakoni@gmail.com>
2025-04-14 03:50:35 +0100

    add error messages

---
+++ src/main/java/com/nouah/revlo/constants/ErrorMessages.java
@@ -0,0 +1,6 @@
+package com.nouah.revlo.constants;
+
+public class ErrorMessages {
+    public static final String EMPTY_INPUT_ERROR = " %s Field cannot be empty. Please provide an input";
+    public static final String UNDEFINED = "UNDEFINED";
+}
