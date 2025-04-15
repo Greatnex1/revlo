f96693013151d396208083985b00889b7031f0bb
Greatnex1 <noahgreatakoni@gmail.com>
2025-04-15 09:02:32 +0100

    refactored error message

--- src/main/java/com/nouah/revlo/constants/ErrorMessages.java
+++ src/main/java/com/nouah/revlo/constants/ErrorMessages.java
@@ -3,4 +3,7 @@ package com.nouah.revlo.constants;
 public class ErrorMessages {
     public static final String EMPTY_INPUT_ERROR = " %s Field cannot be empty. Please provide an input";
     public static final String UNDEFINED = "UNDEFINED";
+    public static final String REQUEST_PROCESSED = "Request processed successfully";
+    public static final String USERNAME_ALREADY_EXIST= "username already exists";
+
 }
