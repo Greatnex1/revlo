a92ad12c3718e41013ff03654f67c4fd8dba4142
Greatnex1 <noahgreatakoni@gmail.com>
2025-04-15 22:48:59 +0100

    add security

---
+++ src/main/java/com/nouah/revlo/config/ApplicationSecurityConfig.java
@@ -0,0 +1,57 @@
+package com.nouah.revlo.config;
+
+import com.nouah.revlo.models.entity.AppUser;
+import com.nouah.revlo.repository.AppUserRepository;
+import com.nouah.revlo.security.TrustedUser;
+import com.nouah.revlo.service.implementation.AppUserService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@AllArgsConstructor
+@Configuration
+@Slf4j
+public class ApplicationSecurityConfig {
+
+    private final AppUserService userService;
+    private final PasswordEncoder passwordEncoder;
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return email -> {
+            try{
+                AppUser user = userService.loadUser(email);
+                return new TrustedUser(user);
+            } catch (IllegalArgumentException e) {
+                log.info("User not found!!");
+                throw new RuntimeException(e);
+            }
+        };
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder);
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+
+
+}
+
