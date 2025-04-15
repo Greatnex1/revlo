054b201b43c14026622458bc1fdca4f6e2f1dc91
Greatnex1 <noahgreatakoni@gmail.com>
2025-04-15 21:49:39 +0100

    add auth request

--- src/main/java/com/nouah/revlo/dto/request/AuthenticationRequest.java
+++ src/main/java/com/nouah/revlo/dto/request/AuthenticationRequest.java
@@ -1,10 +1,13 @@
 package com.nouah.revlo.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 
 @Builder
 public record AuthenticationRequest(
+        @NotBlank(message="this field is required")
         String username,
+        @NotBlank(message="this field is required")
         String password
 ) {
 }
