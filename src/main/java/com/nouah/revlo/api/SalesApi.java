402cb437a28fdfa493c18d9789f483e5624f5584
Greatnex1 <noahgreatakoni@gmail.com>
2025-04-16 07:15:45 +0100

    add api-error

---
+++ src/main/java/com/nouah/revlo/api/SalesApi.java
@@ -0,0 +1,77 @@
+package com.nouah.revlo.api;
+
+import com.nouah.revlo.dto.ResponseDto;
+import com.nouah.revlo.dto.SalesDto;
+import com.nouah.revlo.dto.SalesReportDto;
+import com.nouah.revlo.dto.SalesUpdateDto;
+import com.nouah.revlo.exception.RevloException;
+import com.nouah.revlo.models.entity.Sales;
+import com.nouah.revlo.service.implementation.SalesService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.Instant;
+import java.util.List;
+
+import static com.nouah.revlo.constants.ErrorMessages.REQUEST_FAILED;
+import static com.nouah.revlo.constants.ErrorMessages.REQUEST_PROCESSED;
+import static com.nouah.revlo.constants.UrlConstant.URL_CONSTANT;
+
+
+@AllArgsConstructor
+@RestController
+@RequestMapping(URL_CONSTANT + "/sales")
+public class SalesApi {
+
+    private SalesService salesService;
+
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createSales(@Valid @RequestParam Long userId, @Valid @RequestBody SalesDto salesDto) throws RevloException {
+        boolean isCreated = salesService.createSales(userId,salesDto);
+        if (isCreated){
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(REQUEST_PROCESSED, 200,true, Instant.now()));
+        }else {
+            return ResponseEntity
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(REQUEST_FAILED, 417,false, Instant.now()));
+        }
+    }
+    @GetMapping
+    public ResponseEntity<Sales> findATransaction(@Valid @RequestParam long salesId) throws RevloException {
+        Sales sales = salesService.findSalesById(salesId);
+        return ResponseEntity.status(HttpStatus.OK).body(sales);
+    }
+    @PutMapping("/update")
+    public ResponseEntity<ResponseDto> updateSales(@Valid @RequestParam long salesId,
+                                                   @Valid @RequestParam long userId,
+                                                   @Valid @RequestBody SalesUpdateDto salesUpdateDto) throws RevloException {
+        boolean isUpdated = salesService.updateSales(salesId,userId,salesUpdateDto);
+        if (isUpdated){
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(REQUEST_PROCESSED, 200,true, Instant.now()));
+        }else {
+            return ResponseEntity
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(REQUEST_FAILED, 417,false, Instant.now()));
+        }
+    }
+    @GetMapping("/all/transactions")
+    public ResponseEntity<List<Sales>> getAllTransactions(){
+        List<Sales> sales = salesService.getAllSales();
+        return ResponseEntity.status(HttpStatus.OK).body(sales);
+    }
+    @GetMapping("/reports/sales")
+    public ResponseEntity<SalesReportDto> getSalesReport(@Valid @RequestParam String startDate,
+                                                         @Valid @RequestParam String endDate) {
+        SalesReportDto salesReport = salesService.generateSalesReport(startDate, endDate);
+        return ResponseEntity.status(HttpStatus.OK).body(salesReport);
+    }
+}
+
+
---
+++ src/main/java/com/nouah/revlo/exception/APIError.java
@@ -0,0 +1,33 @@
+package com.nouah.revlo.exception;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Data
+@Builder
+public class APIError {
+    private HttpStatus status;
+
+    private String message;
+
+
+    public APIError(HttpStatus status,String message){
+        this.status =  status;
+        this.message = message;
+    }
+
+    public String convertToJson() throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return mapper.writeValueAsString(this);
+    }
+
+}
