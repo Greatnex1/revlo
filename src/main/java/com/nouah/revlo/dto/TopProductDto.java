ee51137aa8df6df59a96978b1f4e4fe6397772c9
Greatnex1 <noahgreatakoni@gmail.com>
2025-04-16 01:29:29 +0100

    add top product dto

---
+++ src/main/java/com/nouah/revlo/dto/TopProductDto.java
@@ -0,0 +1,19 @@
+package com.nouah.revlo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class TopProductDto {
+    private Long productId;
+    private String productName;
+    private int unitsSold;
+    private BigDecimal revenue;
+}
