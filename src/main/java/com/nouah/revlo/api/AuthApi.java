8215e6aea29c656687230900ef312566028549f3
Greatnex1 <noahgreatakoni@gmail.com>
2025-04-16 17:29:57 +0100

    refactored auth api

--- src/main/java/com/nouah/revlo/api/AuthApi.java
+++ src/main/java/com/nouah/revlo/api/AuthApi.java
@@ -2,7 +2,6 @@ package com.nouah.revlo.api;
 
 import com.nouah.revlo.dto.request.AuthenticationRequest;
 import com.nouah.revlo.dto.response.AuthenticationResponse;
-import com.nouah.revlo.exception.RevloException;
 import com.nouah.revlo.service.interfaces.AuthUseCase;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
@@ -11,12 +10,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import static com.nouah.revlo.constants.UrlConstant.AUTH_URL;
+import static com.nouah.revlo.constants.UrlConstant.URL_CONSTANT;
 
 @AllArgsConstructor
 @RestController
-@RequestMapping(AUTH_URL)
+@RequestMapping(URL_CONSTANT + "/auth")
+
 public class AuthApi {
     private AuthUseCase authService;
     @PostMapping("/login")
