35d16fc6b68644f22e694d2add53caccc14934b6
Greatnex1 <noahgreatakoni@gmail.com>
2025-04-16 07:09:19 +0100

    refactored product repository

--- src/main/java/com/nouah/revlo/repository/ProductRepository.java
+++ src/main/java/com/nouah/revlo/repository/ProductRepository.java
@@ -2,6 +2,15 @@ package com.nouah.revlo.repository;
 
 import com.nouah.revlo.models.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.math.BigDecimal;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    @Query("SELECT p.totalUnitsSoldBy FROM Product p WHERE p.id = :id")
+    Integer getTotalUnitsSoldById(@Param("id") long id);
+
+    @Query("SELECT p.totalRevenue FROM Product p WHERE p.id = :id")
+    BigDecimal getTotalRevenueById(@Param("id") long id);
 }
