060fcf3465af0af0196e8d668d79fcaeef278860
Greatnex1 <noahgreatakoni@gmail.com>
2025-04-16 17:32:50 +0100

    add client test

---
+++ src/test/java/com/nouah/revlo/service/implementation/ClientServiceTest.java
@@ -0,0 +1,87 @@
+package com.nouah.revlo.service.implementation;
+
+import com.nouah.revlo.dto.ClientDto;
+import com.nouah.revlo.models.entity.AppUser;
+import com.nouah.revlo.models.entity.Client;
+import com.nouah.revlo.models.enums.Authority;
+import com.nouah.revlo.repository.AppUserRepository;
+import com.nouah.revlo.repository.ClientRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+@Slf4j
+class ClientServiceTest {
+
+
+    @Mock
+    private ClientRepository clientRepository;
+
+    @Mock
+    private AppUserRepository userRepository;
+
+    @InjectMocks
+    private ClientService clientService;
+
+    ClientDto client;
+    AppUser user;
+
+
+    @BeforeEach
+    void setUp() {
+   user =  AppUser.builder()
+           .firstName("John")
+           .lastName("Doe")
+           .phoneNumber("090345678")
+           .username("Johnny")
+           .build();
+
+   client = ClientDto.builder()
+           .firstName("Johnny")
+           .lastName("Dogs")
+           .email("johnny@gmail.com")
+           .phoneNumber("090565678")
+           .authority(Authority.CLIENT.name())
+           .address("678, London Street, Region")
+           .build();
+    }
+
+    @Test
+    void testThatClientCanBeOnboarded() {
+        when(userRepository.findById(2L)).thenReturn(Optional.of(user));
+        when(clientRepository.save(any())).thenReturn(new Client());
+        assertDoesNotThrow(() -> clientService.addClient(2L, client));
+
+    }
+
+    @Test
+    void testThatClientCannotBeRegisteredWithAnInvalidPhoneNumberThrowsIllegalArgumentException() {
+       ClientDto  client1 = ClientDto.builder()
+               .phoneNumber("123456788990")
+                         .build();
+                when(userRepository.findById(2L)).thenReturn(java.util.Optional.of(user));
+        assertThrows(IllegalArgumentException.class, () -> clientService.addClient(2L, client1));
+    }
+
+
+    @Test
+    void getAllClient() {
+        List<Client> clients = new ArrayList<>();
+        clients.add(new Client());
+        when(clientRepository.findAll()).thenReturn(clients);
+        assertEquals(clients, clientService.getAllClient());
+    }
+}
