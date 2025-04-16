052a591513d41aa20cf29392d93347a24987f350
Greatnex1 <noahgreatakoni@gmail.com>
2025-04-16 17:19:31 +0100

    refactored url constant

--- src/main/java/com/nouah/revlo/constants/UrlConstant.java
+++ src/main/java/com/nouah/revlo/constants/UrlConstant.java
@@ -1,8 +1,6 @@
 package com.nouah.revlo.constants;
 
 public class UrlConstant {
-    public static final String USER_URL = "api/v1/users";
-    public static final String CLIENT_URL = "/api/v1/clients";
-    public static final String AUTH_URL = "/api/v1/auth";
+   public static final String URL_CONSTANT = "/api/v1";
 
 }
