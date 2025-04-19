282e8cfc0fea071f6d372a88a81c0ee91ab1d210
Greatnex1 <noahgreatakoni@gmail.com>
2025-04-19 17:14:49 +0100

    refactor user repo

--- src/main/java/com/nouah/revlo/repository/AppUserRepository.java
+++ src/main/java/com/nouah/revlo/repository/AppUserRepository.java
@@ -2,10 +2,12 @@ package com.nouah.revlo.repository;
 
 import com.nouah.revlo.models.entity.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+    @Query("SELECT u FROM AppUser u WHERE LOWER(u.username) = LOWER(:username)")
     Optional<AppUser> findByUsername(String username);
     AppUser findUserByUsernameIgnoreCase(String username);
 
