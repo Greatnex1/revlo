83174bce206af074f867ecec51144d20041c3013
Greatnex1 <noahgreatakoni@gmail.com>
2025-04-19 05:38:24 +0100

    refactor client entity

--- src/main/java/com/nouah/revlo/models/entity/Client.java
+++ src/main/java/com/nouah/revlo/models/entity/Client.java
@@ -46,7 +46,7 @@ public class Client {
     @Column(insertable = false)
     private LocalDateTime dateLastModified;
     @CreatedBy
-    @Column(nullable = false, updatable = false)
+    @Column(updatable = false)
     private Long createdBy;
     @LastModifiedBy
     @Column(insertable = false)
