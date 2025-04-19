9ed3d917adaf942563a667fde4578d43524abd70
Greatnex1 <noahgreatakoni@gmail.com>
2025-04-19 18:44:35 +0100

    add a json time format display

--- src/main/java/com/nouah/revlo/dto/ResponseDto.java
+++ src/main/java/com/nouah/revlo/dto/ResponseDto.java
@@ -1,14 +1,17 @@
 package com.nouah.revlo.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 
 import java.time.Instant;
+import java.time.ZonedDateTime;
 
 public record ResponseDto(
         String message,
         int status,
         boolean isSuccessful,
-        Instant timestamp
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+        ZonedDateTime timestamp
 
 ) {
 }
