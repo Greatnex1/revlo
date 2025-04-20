6ed03fb7a4754975ff07cc783506ee6fcec84dd6
Greatnex1 <noahgreatakoni@gmail.com>
2025-04-21 00:32:21 +0100

    add product use case

--- src/main/java/com/nouah/revlo/service/interfaces/ProductUseCase.java
+++ src/main/java/com/nouah/revlo/service/interfaces/ProductUseCase.java
@@ -2,13 +2,14 @@ package com.nouah.revlo.service.interfaces;
 
 import com.nouah.revlo.dto.ProductDto;
 import com.nouah.revlo.dto.ProductReportDto;
+import com.nouah.revlo.dto.response.ProductResponse;
 import com.nouah.revlo.exception.RevloException;
 import com.nouah.revlo.models.entity.Product;
 
 import java.util.List;
 
 public interface ProductUseCase {
-    void createProduct(ProductDto productDto);
+    ProductResponse createProduct(ProductDto productDto);
     boolean updateProduct(long productId, ProductDto productDto) throws RevloException;
     Product findProductById(long productId) throws RevloException;
     List<Product> getAllProduct();
