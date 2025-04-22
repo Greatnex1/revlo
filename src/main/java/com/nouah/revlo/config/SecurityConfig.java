52344f1b0e7ee3d648e1fc31a2b207b52bac3a1f
Greatnex1 <noahgreatakoni@gmail.com>
2025-04-22 01:55:40 +0100

    add jpa auditing

--- src/main/java/com/nouah/revlo/config/SecurityConfig.java
+++ src/main/java/com/nouah/revlo/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -26,6 +27,7 @@ import java.util.List;
 @EnableWebSecurity
 @RequiredArgsConstructor
 @EnableMethodSecurity
+@EnableJpaAuditing
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthFilter;
