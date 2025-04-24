ad030467a48d169d55ec70bece586fe1c6a616a1
Greatnex1 <noahgreatakoni@gmail.com>
2025-04-24 02:15:21 +0100

    add serializable

--- src/main/java/com/nouah/revlo/models/entity/Product.java
+++ src/main/java/com/nouah/revlo/models/entity/Product.java
@@ -11,6 +11,7 @@ import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
@@ -21,7 +22,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
-public class Product {
+public class Product implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
