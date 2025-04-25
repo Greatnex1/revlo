c5d569d0140907eca41129bb65acbdefcea7ce2d
Greatnex1 <noahgreatakoni@gmail.com>
2025-04-26 00:01:54 +0100

    add view all client

--- src/main/java/com/nouah/revlo/service/interfaces/ClientUseCase.java
+++ src/main/java/com/nouah/revlo/service/interfaces/ClientUseCase.java
@@ -4,7 +4,8 @@ import com.nouah.revlo.dto.ClientDto;
 import com.nouah.revlo.dto.ClientReportDto;
 import com.nouah.revlo.exception.RevloException;
 import com.nouah.revlo.models.entity.Client;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 public interface ClientUseCase {
@@ -12,6 +13,7 @@ public interface ClientUseCase {
     boolean updateClient(String phoneNumber, ClientDto clientDto) throws RevloException;
     Client findClientByPhoneNumber(String phoneNumber) throws RevloException;
     List<Client> getAllClient();
+    Page<Client> viewAllClients(Pageable pageable) throws RevloException;
     ClientReportDto generateClientReport();
     boolean removeClient(String phoneNumber) throws RevloException;
 }
