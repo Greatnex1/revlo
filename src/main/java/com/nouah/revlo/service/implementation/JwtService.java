a0b50056faaee753ff60912be82a28faf6d23c26
Greatnex1 <noahgreatakoni@gmail.com>
2025-04-26 14:31:41 +0100

    add token expiration time to 30 minutes

--- src/main/java/com/nouah/revlo/service/implementation/JwtService.java
+++ src/main/java/com/nouah/revlo/service/implementation/JwtService.java
@@ -24,7 +24,8 @@ public class JwtService implements JwtUseCase {
 
     @Value("${application.security.jwt.secret-key}")
     private String SECRET_KEY;
-
+    @Value("${application.security.jwt.expiration}")
+    private long jwtExpiration;
     public String extractUsername(String token){
         return extractClaim(token, Claims::getSubject);
     }
@@ -37,20 +38,46 @@ public class JwtService implements JwtUseCase {
     public String generateToken(UserDetails userDetails){
         return generateToken(new HashMap<>(),userDetails);
     }
+//    private boolean isTokenExpired(String token) {
+//        return extractExpiration(token).before(new Date());
+//    }
+//    private Date extractExpiration(String token) {
+//        return extractClaim(token, Claims::getExpiration);
+//    }
+//    private String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+//        return buildToken(extraClaims, userDetails, jwtExpiration);
+//    }
+//
+//    private String buildToken(
+//            Map<String, Object> extraClaims,
+//            UserDetails userDetails,
+//            long expiration
+//    ) {
+//        return Jwts
+//                .builder()
+//                .setClaims(extraClaims)
+//                .setSubject(userDetails.getUsername())
+//                .setIssuedAt(new Date(System.currentTimeMillis()))
+//                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+//               .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+//                .compact();
+//    }
 
-    public String generateToken(
-            Map<String, Object> extraClaims,
-            UserDetails userDetails
-    ){
+
+    public String generateToken(Map<String, Object> extraClaims,
+            UserDetails userDetails )
+    {
         return Jwts
                 .builder()
                 .setClaims(extraClaims)
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 86_400_000))
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
+
+
 
     public boolean isTokenValid(String token, UserDetails userDetails){
         final String username = extractUsername(token);
