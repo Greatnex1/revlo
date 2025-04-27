63a5425f363ba7161f2e810bacee6f96e8f74b2c
Greatnex1 <noahgreatakoni@gmail.com>
2025-04-27 01:29:23 +0100

    refactor sales repo

--- src/main/java/com/nouah/revlo/repository/SalesRepository.java
+++ src/main/java/com/nouah/revlo/repository/SalesRepository.java
@@ -2,6 +2,13 @@ package com.nouah.revlo.repository;
 
 import com.nouah.revlo.models.entity.Sales;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
 
 public interface SalesRepository extends JpaRepository<Sales, Long> {
+    @Query("SELECT SUM(s.quantity) FROM Sales s WHERE s.productId = :productId AND s.dateCreated >= :startDate")
+    Integer getSalesForLastNDays(@Param("productId") Long productId, @Param("startDate") LocalDate startDate);
 }
+
