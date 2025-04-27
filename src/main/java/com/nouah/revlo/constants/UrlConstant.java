cfaab3f745d0a6e2cbe060c95e16dc5b610f7d22
Greatnex1 <noahgreatakoni@gmail.com>
2025-04-27 19:01:14 +0100

    update url

--- src/main/java/com/nouah/revlo/constants/UrlConstant.java
+++ src/main/java/com/nouah/revlo/constants/UrlConstant.java
@@ -2,5 +2,39 @@ package com.nouah.revlo.constants;
 
 public class UrlConstant {
    public static final String URL_CONSTANT = "/api/v1";
+   public static final String SIGNUP = "/api/v1/users/register";
+   public static final String LOGIN = "/api/v1/auth/login";
+   public static final String CLIENT_ONBOARDING =    "/api/v1/clients/";
+   public static final String UPDATE_CLIENT_DETAILS =    "/api/v1/clients/update";
+   public static final String ALL_CLIENT =    "/api/v1/clients";
+   public static final String FETCH_ALL_CLIENT =    "/api/v1/clients/view-all";
+   public static final String SEARCH_FOR_A_CLIENT =    "/api/v1/clients/search";
+   public static final String CLIENT_REPORT =    "/api/v1/clients/reports/client";
+   public static final String REMOVE_CLIENT =    "/api/v1/clients/delete";
+   public static final String CREATE_SALES =    "/api/v1/sales/create";
+   public static final String UPDATE_SALES =    "/api/v1/sales/create";
+   public static final String ALL_SALES =    "/api/v1/sales";
+   public static final String SALES_REPORT =    "/api/v1/sales/report";
+   public static final String CREATE_PRODUCT =    "/api/v1/products/create";
+   public static final String UPDATE_PRODUCT =    "/api/v1/products/update";
+   public static final String FETCH_PRODUCTS =    "/api/v1/products/view-all";
+   public static final String SEARCH_PRODUCT =    "/api/v1/products/search";
+   public static final String REMOVE_PRODUCT =    "/api/v1/products/delete";
+   public static final String PRODUCT_REPORT =    "/api/v1/products/reports/product";
+   public static final String ALL_PRODUCTS =    "/api/v1/products";
 
+
+//   /api/v1/sales/pdf
+//   /api/v1/sales/reports/sales
+//   /api/v1/sales/all/transactions
+//   /api/v1/products/update
+//   /api/v1/products/create
+//   /api/v1/products
+//   /api/v1/products/view-all
+//
+//   /api/v1/products/search
+//
+//   /api/v1/products/delete
+
+//   /api/v1/clients/delete
 }
