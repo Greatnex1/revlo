affe625292a354b0290ffa5a321a21a72debe464
Greatnex1 <noahgreatakoni@gmail.com>
2025-05-02 20:56:17 +0100

    refactor auth api

--- src/main/java/com/nouah/revlo/api/AuthApi.java
+++ src/main/java/com/nouah/revlo/api/AuthApi.java
@@ -2,15 +2,14 @@ package com.nouah.revlo.api;
 
 import com.nouah.revlo.dto.request.AuthenticationRequest;
 import com.nouah.revlo.dto.response.AuthenticationResponse;
+import com.nouah.revlo.security.RateLimitService;
 import com.nouah.revlo.service.interfaces.AuthUseCase;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import static com.nouah.revlo.constants.UrlConstant.URL_CONSTANT;
 
 @AllArgsConstructor
@@ -20,9 +19,12 @@ public class AuthApi {
 
     private AuthUseCase authService;
 
+
     @PostMapping("/login")
     @Operation(summary = "Login", description = "generates token for user after login")
+
     public ResponseEntity<AuthenticationResponse> login(@RequestBody @Valid AuthenticationRequest loginDto) {
+
     return ResponseEntity.ok(authService.authenticate(loginDto));
     }
 }
