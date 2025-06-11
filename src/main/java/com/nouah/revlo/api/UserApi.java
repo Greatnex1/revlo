0e2837a621b97937c5ed2bad6c88869bbc3c2c14
Greatnex1 <noahgreatakoni@gmail.com>
2025-06-11 22:14:43 +0100

    refactor

--- src/main/java/com/nouah/revlo/api/UserApi.java
+++ src/main/java/com/nouah/revlo/api/UserApi.java
@@ -32,7 +32,7 @@ import static com.nouah.revlo.constants.UrlConstant.URL_CONSTANT;
 public class UserApi {
     private AppUserService userService;
 
-    @PostMapping("/register")
+    @PostMapping(URL_C)
     @Operation(summary= "Create a new user")
     public ResponseEntity<ResponseDto> createAccount(@RequestBody @Valid AppUserDto userDto) {
         userService.userRegistration(userDto);
